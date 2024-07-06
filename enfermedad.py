358ac19f4ff4a975795ea15c5bf92e9b688c8a59
Ciswell <125753796+Ciswell@users.noreply.github.com>
2024-07-06 12:11:21 -0400

    enfermedad
    "Define la clase Enfermedad con probabilidades de infección y recuperación"

---
+++ enfermedad.py
@@ -0,0 +1,4 @@
+class Enfermedad:
+    def __init__(self, infeccion_probable, recuperacion_probable):
+        self.infeccion_probable = infeccion_probable
+        self.recuperacion_probable = recuperacion_probable
