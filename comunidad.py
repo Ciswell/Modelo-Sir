c34b8edecbd0e9b6363cd7676ef863ae990aa0cf
Ciswell <125753796+Ciswell@users.noreply.github.com>
2024-07-07 10:40:14 -0400

    Inicia comunidad con ciudadanos y contacto diario
    - "Añadí método para crear ciudadanos e infectar un subconjunto inicialmente"
    - "Añadí un método para actualizar contactos diarios con un número aleatorio de interacciones (0 a 8)"

--- comunidad.py
+++ comunidad.py
@@ -1,4 +1,5 @@
 from ciudadano import Ciudadano
+import random
 
 class Comunidad:
     def __init__(self, num_ciudadanos, enfermedad, num_infectados):
@@ -6,10 +7,21 @@ class Comunidad:
         self.ciudadanos = self.crear_ciudadanos(num_ciudadanos, num_infectados)
 
     def crear_ciudadanos(self, num_ciudadanos, num_infectados):
-        ciudadanos = [Ciudadano(id=i, comunidad=self) for i in range(num_ciudadanos)] #el rango lo llama desde el main
+        ciudadanos = [Ciudadano(id=i, comunidad=self) for i in range(num_ciudadanos)]
 
-        #Infectar los primeros num_infectados ciudadanos de manera determinística
-        for i in range(num_infectados):
-            ciudadanos[i].estado = 'I'
+        # Infectar los primeros num_infectados ciudadanos
+        infectados_iniciales = random.sample(ciudadanos, num_infectados)
+        for infectado in infectados_iniciales:
+            infectado.estado = 'I'
 
         return ciudadanos
+
+    def actualizar_contactos(self):
+        for ciudadano in self.ciudadanos:
+            ciudadano.contactos = []
+            num_contactos = random.randint(0, 8)  #Permitir entre 0 y 8 contactos
+            contactos = random.sample(self.ciudadanos, k=num_contactos)
+            for contacto in contactos:
+                if contacto != ciudadano:
+                    ciudadano.agregar_contacto(contacto)
+                    contacto.agregar_contacto(ciudadano)
