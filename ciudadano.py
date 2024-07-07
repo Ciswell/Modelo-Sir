d3d0284ef88a05730b987be6522adf62d20870d0
Ciswell <125753796+Ciswell@users.noreply.github.com>
2024-07-07 10:09:19 -0400

    sistema de contacto
    "Cree un método que se encuentra en la clase Ciudadano y su función es añadir un contacto a la lista de "contactos" de un ciudadano"

--- ciudadano.py
+++ ciudadano.py
@@ -2,5 +2,10 @@ class Ciudadano:
     def __init__(self, id, comunidad, estado='S'):
         self.id = id
         self.comunidad = comunidad
-        self.estado = estado  #'S' para susceptible, 'I' para infectado, 'R' para recuperado
+        self.estado = estado  # 'S' para susceptible, 'I' para infectado, 'R' para recuperado
         self.pasos_infectado = 0
+        self.contactos = []  # Lista de contactos directos
+
+    def agregar_contacto(self, contacto):
+        if contacto not in self.contactos:
+            self.contactos.append(contacto)
