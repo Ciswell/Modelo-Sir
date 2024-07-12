9c0308a2c67535b0573c4a266d2e37e98755659c
Ciswell <125753796+Ciswell@users.noreply.github.com>
2024-07-12 13:09:27 -0400

    eliminación de listas innecesarias
    fueron eliminadas las listas nuevos_infectados [ ] y nuevos_recuperados [ ] que originalmente eran unas listas "transportadoras" de estados

--- simulador.py
+++ simulador.py
@@ -1,12 +1,12 @@
 import random
-import numpy as np  # Importar numpy
+import numpy as np
 
 class Simulador:
-    def __init__(self):
-        self.susceptibles = np.array([])  # Usar numpy arrays
-        self.infectados = np.array([])    # Usar numpy arrays
-        self.recuperados = np.array([])   # Usar numpy arrays
-        self.umbral_infeccion = 1  # Umbral de interacciones necesarias para infectar
+    def __init__(self, umbral_infeccion=1):
+        self.susceptibles = np.array([])
+        self.infectados = np.array([])
+        self.recuperados = np.array([])
+        self.umbral_infeccion = umbral_infeccion  # Umbral de interacciones necesarias para infectar
 
     def set_comunidad(self, comunidad):
         self.comunidad = comunidad
@@ -20,35 +20,18 @@ class Simulador:
             self.simular_paso()
 
     def simular_paso(self):
-        nuevos_infectados = []
-        nuevos_recuperados = []
-
-        # Calcular número de nuevos infectados basado en contactos directos
-        tasa_infeccion = self.comunidad.enfermedad.infeccion_probable * 0.5  # Ajustar la tasa de infección
+        tasa_infeccion = self.comunidad.enfermedad.infeccion_probable
         for infectado in self.infectados:
             for contacto in infectado.contactos_diarios:
                 if contacto.estado == 'S':
-                    # Contar interacciones con infectados
-                    interacciones_infectados = sum(1 for i in contacto.contactos_diarios if i.estado == 'I')
+                    interacciones_infectados = len([i for i in contacto.contactos_diarios if i.estado == 'I'])
                     if interacciones_infectados >= self.umbral_infeccion and random.random() < tasa_infeccion:
-                        nuevos_infectados.append(contacto)
+                        self.infectar_contacto(contacto)
 
-        # Calcular número de nuevos recuperados
         tasa_recuperacion = self.comunidad.enfermedad.recuperacion_probable
-        for infectado in self.infectados:
+        for infectado in list(self.infectados):  # Crear una copia para evitar problemas durante la iteración
             infectado.pasos_infectado += 1
             if infectado.pasos_infectado >= 15 or random.random() < tasa_recuperacion:
-                nuevos_recuperados.append(infectado)
+                self.recuperar_infectado(infectado)
 
-        # Actualización de estados
-        for infectado in nuevos_infectados:
-            if infectado in self.susceptibles:
-                self.susceptibles = np.delete(self.susceptibles, np.where(self.susceptibles == infectado))
-                infectado.estado = 'I'
-                infectado.pasos_infectado = 0
-                self.infectados = np.append(self.infectados, infectado)
-
-        for recuperado in nuevos_recuperados:
-            self.infectados = np.delete(self.infectados, np.where(self.infectados == recuperado))
-            recuperado.estado = 'R'
-            self.recuperados = np.append(self.recuperados, recuperado)
+ 
